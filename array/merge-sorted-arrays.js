fc6e58bc1cfce5e06c491e10e3cc7b62664f720f
13402208 <sourav1.suryawanshi@africa.airtel.com>
2024-01-28 18:32:05 +0530

    merge two arrays code added

---
+++ array/merge-sorted-arrays.js
@@ -0,0 +1,38 @@
+function mergeSortedArrays(array1, array2){
+    if(!array1.length){
+        return array2;
+    }
+    if(!array2.length){
+        return array1;
+    }
+    let i = 0;
+    let j = 0;
+    let mergedArray = [];
+    while(i < array1.length || j<array2.length){
+        
+        if(i >= array1.length && j < array2.length ){
+            while(j < array2.length){
+                mergedArray.push(array2[j]);
+                j++;
+            }
+        }
+        if(j >= array2.length && i < array1.length ){
+            while(i < array1.length){
+                mergedArray.push(array1[i]);
+                i++;
+            }
+        }
+        if(array1[i]<array2[j]){
+            mergedArray.push(array1[i]);
+            i++;
+        }
+        else if(array1[i]>array2[j]){
+            mergedArray.push(array2[j]);
+            j++;
+        }
+        console.log(mergedArray)
+    }
+  return mergedArray;
+
+}
+console.log(mergeSortedArrays([-1,-1,-1,-1],[-2,-2,-2,-2]));
